d64c5195bfec45b248765544ae8fed683f4e1c8d
shatayu <shatayukulkarni@gmail.com>
2024-08-09 17:13:55 -0400

    new models

---
+++ train_dqn_model.py
@@ -0,0 +1,57 @@
+# Create a DQN algorithm object using a config object ..
+from ray.rllib.algorithms.dqn import DQNConfig
+from RLWithBushMostellerEnv import RLWithBushMostellerWholeGameEnv
+import os
+import sys
+from datetime import datetime
+
+NUM_TRAINING_ITERATIONS = 1000
+
+# prop game: /home/shatayu/ray_results/PPO_RLWithBushMostellerWholeGameEnv_2023-05-12_23-16-30p84cj234/checkpoint_001000
+# sum model:  /home/shatayu/ray_results/PPO_RLWithBushMostellerWholeGameEnv_2023-05-13_11-04-37_jmf78e3/checkpoint_001000
+
+if __name__ == "__main__":
+    # Check if the command line argument is provided
+    if len(sys.argv) < 2 or sys.argv[1] not in ('proportion', 'sum'):
+        print("Please provide the reward function ('proportion' or 'sum')")
+    else:
+        # Extract the checkpoint location from the command line argument
+        reward_function = sys.argv[1]
+        prefix = sys.argv[2]
+
+        my_dqn = ( 
+            DQNConfig()
+            .rollouts(num_rollout_workers=1)
+            .resources(num_gpus=0)
+            .environment(
+                env=RLWithBushMostellerWholeGameEnv,
+                env_config={
+                    'num_rounds_hidden': 0,
+                    'reward_function': reward_function
+                },
+                auto_wrap_old_gym_envs=False,
+            )
+            .build()
+        )
+
+
+        for i in range(NUM_TRAINING_ITERATIONS):
+            my_dqn.train()
+
+            if (i + 1) % 10 == 0:
+                path_to_checkpoint = my_dqn.save()
+                print("*********************************")
+                print(
+                    "An Algorithm checkpoint has been created inside directory: "
+                    f"'{path_to_checkpoint}'."
+                )
+                print("---------------------------------")
+
+        # Let's terminate the algo for demonstration purposes.
+        final_checkpoint = my_dqn.save('./models/')
+        renamed_checkpoint = f'models/{prefix}_dqn_{NUM_TRAINING_ITERATIONS}_model_{reward_function}_{final_checkpoint.split("/")[-1]}_{datetime.now().strftime("%Y%m%d%H%M%S%f")}'
+
+        os.rename(final_checkpoint, renamed_checkpoint)
+
+        my_dqn.stop()
+        print(f'Final DQN checkpoint (reward function: {reward_function}): {renamed_checkpoint}')
